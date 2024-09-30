01660612f54fe962e9b2dfc78c453bd9f3fe7275
Naeemul Hassan <95282394+NaeemulHassan45@users.noreply.github.com>
2024-10-01 02:17:35 +0500

    css cleanup

--- my-react-app/src/pages/Landing/index.jsx
+++ my-react-app/src/pages/Landing/index.jsx
@@ -2,7 +2,7 @@ const Landing = () => {
   return (
     <div className='flex items-center justify-center'>
       <h1 className='text-4xl font-bold text-sky-300'>
-        Welcome to the landing page!
+        Goodluck Koding!
       </h1>
     </div>
   );
